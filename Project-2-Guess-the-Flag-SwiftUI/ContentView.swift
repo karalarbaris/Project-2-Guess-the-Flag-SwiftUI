e5e4bb768f75d36bc6d94d4202c1437329755043
Baris <karalarbaris@gmail.com>
2021-06-07 10:07:55 +0200

    Initial Commit

---
+++ Project-2-Guess-the-Flag-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-2-Guess-the-Flag-SwiftUI
+//
+//  Created by Baris Karalar on 7.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-2-Guess-the-Flag-SwiftUI/Project_2_Guess_the_Flag_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2_Guess_the_Flag_SwiftUIApp.swift
+//  Project-2-Guess-the-Flag-SwiftUI
+//
+//  Created by Baris Karalar on 7.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_2_Guess_the_Flag_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
